26a4cb86a499ca9c0f93118b29cb4d790636fd70
Kirill Netavskiy <simu4.net@gmail.com>
2017-01-26 15:24:59 +0200

    first commit

---
+++ Test23.01/GraphMaker.swift
@@ -0,0 +1,9 @@
+//
+//  GraphMaker.swift
+//  Test23.01
+//
+//  Created by Kirill Netavskiy on 23.01.17.
+//  Copyright © 2017 Kirill Netavskiy. All rights reserved.
+//
+
+import Foundation
---
+++ Test23.01/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Test23.01
+//
+//  Created by Kirill Netavskiy on 26.01.17.
+//  Copyright © 2017 Kirill Netavskiy. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
